cfe089374b8e11d4372cf95c2dcc28da9ca14550
Willian <vilyanyaumbaev@gmail.com>
2019-09-27 15:22:03 +0300

    project init

---
+++ SocialBank/SocialBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialBank
+//
+//  Created by Вильян Яумбаев on 27/09/2019.
+//  Copyright © 2019 Вильян Яумбаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
